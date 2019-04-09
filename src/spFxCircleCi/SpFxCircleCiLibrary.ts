de37e0f932455f8f6950284336df7990dafa5ccc
shrenky <shrenkyang@gmail.com>
2019-04-09 17:08:31 +0800

    add sum method

--- src/spFxCircleCi/SpFxCircleCiLibrary.ts
+++ src/spFxCircleCi/SpFxCircleCiLibrary.ts
@@ -2,4 +2,8 @@ export default class SpFxCircleCiLibrary {
   public name(): string {
     return 'SpFxCircleCiLibrary';
   }
+
+  public sum(x: number, y: number) : number {
+    return x + y;
+  }
 }
